a785e6c5dd99de2a69fd7cd60757b5bd404094d0
CreativePenguin <panda501wp@gmail.com>
2019-09-20 09:27:38 -0400

    Fix last night's homework, and add the classwork in as well

--- 08_app/ThreeAppRouteApp.py
+++ 08_app/ThreeAppRouteApp.py
@@ -1,12 +1,23 @@
+# Winston Peng
+# SoftDev1 pd 9
+# K08 -- Lemme Flask You Sump'n
+# 2019-09-19
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/pengwin")
-@app.route("/wpeng00")
-
 def hello_world():
     print(__name__)
-    return "No hablo queso!"
+    return "This is the beginning of the page"
+@app.route("/link")
+def triforce():
+    return 'woah look it\'s zelda'
+@app.route("/creedthoughts")
+def parks_and_rec():
+    return 'How do you do fellow kids?'
+if __name__ == '__main__': # Determines if file is being run by itself
+    app.debug = True
+    app.run()
 
