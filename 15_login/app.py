0f2ab2792d610f520020bd5535c7df2b235eca7e
CreativePenguin <panda501wp@gmail.com>
2019-10-03 14:05:50 -0400

    Login works, couldn't figure out cookies

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,20 +1,20 @@
-# Winston Peng
+# Winston Peng & Albert Wan -- Team Dental Hygeine
 # SoftDev1 pd9
 # K15 -- Do I Know You? -- Using Cookies, Login Page
 # 10-2-2019
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 
+user = {}
+
 @app.route("/")
 def occupy():
     print("hello")
-    global username
-    global password
-    username = request.args['username']
-    password = request.args['password']
+#    username = request.args['username']
+#    password = request.args['password']
     print(request.cookies)
-    if request.cookies.get['login']:  # replace with actual conditional
-        redirect('/account')
+#    if request.cookies.get['login']:  # replace with actual conditional
+#        redirect('/account')
     return render_template(
         "page.html",
     )
@@ -26,19 +26,23 @@ logins = {'bertw2002': 'oreo'}
 @app.route("/auth")
 def route():
     """this is a temporary page that checks if login was correct"""
+    username = request.args['username']
+    password = request.args['password']
     print(request.cookies)
-    if logins[username] == password:
-        return redirect('/login')
+    print('decoy')
     if username not in logins.keys():
         return redirect('/newuser')
+    if username == password:
+        return redirect('/login')
     return redirect('/account')
 
 @app.route('/account')
 def login():
     """prints welcome message in account page"""
+    #user = request.args['username']
     return render_template(
-        'account.html',
-        user=username
+        'account.html'
+        #user=username
     )
 
 @app.route('/newuser')
