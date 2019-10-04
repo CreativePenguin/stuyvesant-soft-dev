f244ae53b739c9b2df7d6612b9991c2a6b86336e
Winston Peng <panda501wp@gmail.com>
2019-10-03 22:20:12 -0400

    Complete 15. Use sessions in place of cookies. Add utl folder to manage adding new accounts

--- 15_login/app.py
+++ 15_login/app.py
@@ -3,51 +3,74 @@
 # K15 -- Do I Know You? -- Using Cookies, Login Page
 # 10-2-2019
 from flask import Flask, render_template, request, redirect, session
+from utl import login as login_file
 app = Flask(__name__)
+app.secret_key = 'dentalhygeine'
 
-user = {}
+logins = login_file.getDict()  # keys = usernames, values = passwords
+global error_message  # This variable keeps track of whether it's a username error or a password error
+error_message = None
 
 @app.route("/")
-def occupy():
-    print("hello")
-#    username = request.args['username']
-#    password = request.args['password']
-    print(request.cookies)
-#    if request.cookies.get['login']:  # replace with actual conditional
-#        redirect('/account')
+def root():
+    """ Redirects to login if no existing account, and to welcome page if already logged in """
+    # Adds a new user
+    print(request.args)
+    if 'username' in request.args:
+        login_file.addUser(request.args['username'], request.args['password'])
+    session_user = session['username']
+    session_pwd = session['password']
+    if session_user in logins.keys():  # change so that this will only be true if user is already logged in
+        if session_pwd == logins[session_user]:
+            return redirect('/welcome')
+    return redirect('/login')
+
+@app.route('/login')
+def login():
+    global error_message
+    print(error_message)
     return render_template(
         "page.html",
+        error=error_message  # Prints out whether it's a username error, or password error
     )
-
-
-logins = {'bertw2002': 'oreo'}
-
 
 @app.route("/auth")
 def route():
     """this is a temporary page that checks if login was correct"""
     username = request.args['username']
     password = request.args['password']
+    global error_message
     print(request.cookies)
-    print('decoy')
+    # logins.keys() holds all recognizable usernames
     if username not in logins.keys():
-        return redirect('/newuser')
-    if username == password:
+        error_message = 'Username not found'
+        print(error_message)
         return redirect('/login')
-    return redirect('/account')
+    # checks to see if the dictionary match for the username is the password
+    if logins[username] != password:
+        error_message = 'Incorrect Password'
+        print(error_message)
+        return redirect('/login')
+    # sets error message to none, so that login page will stop saying they have an error
+    error_message = None
+    session['username'] = username
+    session['password'] = password
+    return redirect('/welcome')
 
-@app.route('/account')
-def login():
-    """prints welcome message in account page"""
+@app.route('/welcome')
+def welcome():
+    """prints welcome message in welcome page"""
     #user = request.args['username']
+    global error_message
     return render_template(
-        'account.html'
-        #user=username
+        'welcome.html'
     )
 
 @app.route('/newuser')
 def newuser():
     """This is where people can make a new account"""
+    global error_message
+    error_message = None  # redirects back to "/" without going through other error message checks
     return render_template(
         'newuser.html'
     )
---
+++ 15_login/utl/login.py
@@ -0,0 +1,22 @@
+import csv
+
+readfile = open('utl/logins.csv','r').readlines()
+writefile = open('utl/logins.csv','a+')
+
+def __csvToDict(csv, dic={}):
+    """Converts csv file into dictionary using recursion"""
+    if csv:
+        # arr = list. arr[0] = occupation. arr[1] = percentage
+        arr = csv.pop(0).rstrip('\n').rsplit(',', 1)
+        # transform array indexes to key and value in dictionary
+        dic[arr[0]] = arr[1]
+        return __csvToDict(csv, dic)
+    return dic
+
+def getDict():
+    """returns dictionary containing all the usernames and passwords"""
+    return __csvToDict(readfile)
+
+def addUser(usr, pwd):
+    """Adds a new user"""
+    writefile.write('{},{}\n'.format(usr, pwd))
