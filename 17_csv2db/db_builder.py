ddac3f3b81d28ec06885441369b96b4a2666bc74
CreativePenguin <panda501wp@gmail.com>
2019-10-11 09:26:53 -0400

    Continue working on db_builder.py

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -1,5 +1,5 @@
 #Clyde "Thluffy" Sinclair
-#SoftDev  
+#SoftDev
 #skeleton :: SQLITE3 BASICS
 #Oct 2019
 
@@ -7,14 +7,42 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
 
-DB_FILE="discobandit.db"
+DB_FILE='salmon.db'
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
 #==========================================================
 
+def create_table_sql(cls, table_name, column_names):
+    """Return string that can be used to create SQL data table
+    The column_names should include the DATATYPE as part of the same string
+    """
+    command = 'CREATE TABLE ' + table_name
+    for i in column_names:
+        command += i
+    return command
+
+def insert_into_sql(cls, table_name, column_vals):
+    """Return string that can be used to add rows to SQL data table
+    The column_vals should include the DATATYPE as part of the same string
+    """
+    command = 'INSERT INTO ' + table_name
+    for i in column_vals:
+        command += i
+    return command
+
+def csv_to_table_sql(cls, csv_dict_reader):
+
+
 # < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+with open('students.csv') as studentsFile:
+    with open('courses.csv') as coursesFile:
+        studentsReader = csv.DictReader(studentsFile)
+        coursesReader = csv.DictReader(coursesFile)
+        #c.execute(create_table_sql('students
+        for i in studentsReader:
+
 
 
 command = ""          # test SQL stmt in sqlite3 shell, save as string
