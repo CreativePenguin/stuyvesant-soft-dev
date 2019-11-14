8efd60c928f978363c575899fc481f690c7649dc
Creative Penguin <panda501wp@gmail.com>
2019-11-14 02:48:04 -0500

    idk what I'm doing. I hate this.

---
+++ 24_rest/app.py
@@ -0,0 +1,22 @@
+# Winston Peng
+# SoftDev1 pd9
+# K
+# 2019-9-23
+
+from flask import Flask, render_template, request
+import urllib3, json
+
+app = Flask(__name__)
+nasa_key = 'zUNNcvA48NgnrzwcdqBgI3YGKKFYgfwmVxDf0jHb'
+http = urllib3.PoolManager()
+
+@app.route('/')
+def main():
+    return render_template(
+        'index.html',
+        tmp=http.request
+    )
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
