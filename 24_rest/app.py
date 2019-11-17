9d70d4872c0ab6785d66c9274a5d62889d255d82
Creative Penguin <panda501wp@gmail.com>
2019-11-17 11:27:59 -0500

    I think my api key broke, whoops, not really sure what to do now. I keep getting Max retry error

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,6 +1,6 @@
 # Winston Peng
 # SoftDev1 pd9
-# K
+# K #24: A RESTful Journey Skyward
 # 2019-9-23
 
 from flask import Flask, render_template, request
@@ -9,12 +9,15 @@ import urllib3, json
 app = Flask(__name__)
 nasa_key = 'zUNNcvA48NgnrzwcdqBgI3YGKKFYgfwmVxDf0jHb'
 http = urllib3.PoolManager()
+httprequest = http.request('GET', 'https://api.nasa.gov/planetary/apod?{}{}'.format('api_key=', nasa_key))
+http_dict = json.loads(httprequest.data.decode('utf-8'))
 
 @app.route('/')
 def main():
     return render_template(
         'index.html',
-        tmp=http.request('GET', 'https://api.nasa.gov/planetary/apod?={}'.format(nasa_key))
+        tmp=httprequest,
+        ext=http_dict['explanation']
     )
 
 if __name__ == '__main__':
