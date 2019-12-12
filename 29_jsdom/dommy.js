76009ef6951af6667916952642226a895e14d8cc
Creative Penguin <panda501wp@gmail.com>
2019-12-12 18:24:07 -0500

    Add button functionality

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -1,17 +1,54 @@
 var changeHeading = function(e) {
-    var h = document.getElementById("h");
-    h.innerHTML = e;
+    document.getElementById("h").innerHTML = e;
+};
+
+var removeItem = function(e) {
+    e.remove();
 };
 
 var lis = document.getElementsByTagName("li");
 
 for(var i = 0; i < lis.length; i++) {
+    //TODO: DELETE SOON -- This line just exists to stop errors
     var head = lis[i];
-    lis[i].addEventListener('mouseover', () => {changeHeading(head.innerHTML);});
+    //lis[i].setAttribute("id", `item${i}`);
+    // Can't figure out how to get this code to work
+    lis[i].addEventListener('mouseover', () => {
+        //TODO: Change this line too -- tmp fix
+        changeHeading(head.innerHTML);
+        //changeHeading(lis[i].innerHTML);
+        //changeHeading(document.getElementById(``).innerHTML);
+    });
     lis[i].addEventListener('mouseout', () => {changeHeading("Hello World!");});
+    lis[i].addEventListener('click', () => {
+        removeItem(document.getElementById("test"));
+    });
     console.log(`lis[${i}]`);
 };
 
-var addItem = (e) => {
-    var list = document.getElementById("thelist").inner_html;
+var addItem = (element, value) => {
+    var list = element;
+    var item = document.createElement("li");
+    item.innerHTML = value;
+    list.append(item);
 };
+
+var addFib = function(n) {
+    
+};
+
+var addFib2 = function(n) {
+    console.log(n);
+    if (n == 0) return n;
+    if (n == 1) return n;
+    else {
+        return fib(n-1) + fib(n-2);
+    }
+};
+
+var b = document.getElementById("b");
+b.addEventListener("click", () => {
+    addItem(document.getElementById("thelist"), "WORD");
+});
+var fb = document.getElementById("fb");
+fb.addEventListener("click", addFib);
