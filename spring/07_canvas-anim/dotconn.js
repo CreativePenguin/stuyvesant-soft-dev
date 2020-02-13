946c82a8c656ebdf2d6b6fbab19dc40398dea2c3
CreativePenguin <panda501wp@gmail.com>
2020-02-13 13:55:41 -0500

    Almost works

--- spring/07_canvas-anim/dotconn.js
+++ spring/07_canvas-anim/dotconn.js
@@ -3,29 +3,44 @@ var ctx = canvas.getContext('2d');
 var start_butt = document.getElementById('start');
 var stop_butt = document.getElementById('stop');
 var start = null;
+var draw_animation = null;
+var clear_animation = null;
 var radius = 0;
+var isExpanding = true;
 
 var drawCircle = function(timestamp) {
   if(!start) start = timestamp;
-  var progress = start - timestamp;
-  if(progress < 5) {
-    window.requestAnimationFrame(drawCircle)
+  var progress = timestamp - start;
+  if(progress % 10) {
+    clear_animation = window.requestAnimationFrame(clear);
   }
+  // console.log(`timestamp:${timestamp}; radius:${radius}`);
   drawCircleH(radius);
-  radius += 1;
-}
+  draw_animation = window.requestAnimationFrame(drawCircle);
+  if(radius <= 0) isExpanding = true;
+  if(radius >= canvas.width / 2) isExpanding = false;
+  radius += isExpanding ? 1 : -1;
+};
 
 var drawCircleH = function(radius) {
   ctx.beginPath();
   ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, Math.PI * 2);
   ctx.fill();
-}
+};
+
+var clear = function() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
 
 start_butt.addEventListener('click', () => {
-  console.log("press");
-  window.requestAnimationFrame(drawCircle);
-  radius + 1;
-})
-// document.addEventListener('click', () => {
-//   ctx.cancelAnimationFrame();
-// });
+  animation = window.requestAnimationFrame(drawCircle);
+  console.log(animation);
+});
+stop_butt.addEventListener('click', () => {
+  window.cancelAnimationFrame(draw_animation);
+  window.cancelAnimationFrame(clear_animation);
+});
+document.getElementById('clear').addEventListener('click', () => {
+  console.log(animation);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
