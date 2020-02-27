3cdfa7f27df13fab2b4b52960745496170c4ba50
Winston Peng <panda501wp@gmail.com>
2020-02-27 11:15:51 +0000

    Not quite sure what to do next

--- spring/09_mongo/app.py
+++ spring/09_mongo/app.py
@@ -5,12 +5,14 @@
 
 from flask import Flask, render_template, request, session
 from pymongo import MongoClient
-# import pymongo
+import pymongo
 
 app = Flask(__name__)
 f1 = open('primer-dataset.json', 'r')
 client = MongoClient()
-db = client['restaurants']
+db = client['09_mongo']
+restaurant = db['restaurants']
+restaurant_id = restaurant.insert_one(f1).inserted_id()
 
 @app.route('/')
 def main():
@@ -22,10 +24,10 @@ def main():
 def filter():
     if request.method == "POST":
         borough = request.form['borough']
-        zip = request.form['zip']
+        zipcode = request.form['zip']
         grade = request.form['grade']
         score = request.form['score']
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run()
