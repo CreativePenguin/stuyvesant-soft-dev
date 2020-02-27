49b8dbb503fe1f56376441a8816525ea5032aa73
Creative Penguin <panda501wp@gmail.com>
2020-02-27 02:13:51 -0500

    Yeet

--- spring/09_mongo/app.py
+++ spring/09_mongo/app.py
@@ -6,16 +6,23 @@
 from flask import Flask, render_template, request, session
 from pymongo import MongoClient
 import pymongo
+import json
 
 app = Flask(__name__)
-f1 = open('primer-dataset.json', 'r')
 client = MongoClient()
 db = client['09_mongo']
 restaurant = db['restaurants']
-restaurant_id = restaurant.insert_one(f1).inserted_id()
+# f1 = open('primer-dataset.json', 'r')
+# json.load(f1.readlines())
+# f1.close()
+with open('primer-dataset.json') as f:
+    filedata = json.load(f)
+restaurant_id = restaurant.insert_one(filedata).inserted_id()
+
 
 @app.route('/')
 def main():
+    print(restaurant_id.find({}))
     return render_template(
             'foo.html'
             )
@@ -28,6 +35,7 @@ def filter():
         grade = request.form['grade']
         score = request.form['score']
 
+client.close()
 # if __name__ == '__main__':
 #     app.debug = True
 #     app.run()
