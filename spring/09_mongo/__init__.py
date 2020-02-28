ca496ffc2ff7c8e2ed1ea6351627b56026059597
Winston Peng <panda501wp@gmail.com>
2020-02-28 19:07:08 +0000

    Haven't tested it, but hopefully it works

--- spring/09_mongo/__init__.py
+++ spring/09_mongo/__init__.py
@@ -16,14 +16,17 @@ restaurant = db['restaurants']
 # json.load(f1.readlines())
 # f1.close()
 with open('primer-dataset.json') as f:
-    filedata = json.load(f)
-restaurant_id = restaurant.insert_one(filedata).inserted_id()
+    for line in f.readline():
+        restaurant_id = restaurant.insert_one(line).inserted_id()
+# with open('primer-dataset.json') as f:
+#     filedata = json.load(f)
+# restaurant_id = restaurant.insert_one(filedata).inserted_id()
 
 
 @app.route('/')
 def main():
     print(restaurant_id.find({}, {'_id': 0}))
-    restaurant_info = restaurant_id.find({}, {'_id': 0})
+    restaurant_info = restaurant_id.find({}, {'_id': 0}) if session['filtered_data'] is None else session['filtered_data']
     return render_template(
         'foo.html',
         data=restaurant_info
@@ -33,13 +36,16 @@ def main():
 def filter():
     if request.method == "POST":
         filter_params = {}
-        zipcode = request.form['zip']
-        grade = request.form['grade']
-        score = request.form['score']
-        if not request.form['borough'] is None:
-            filter_params['borough'] = borough
+        filter_params['address'] = {}
+        filter_params['date'] = {}
+        filter_params['borough'] = request.form['borough']
+        filter_params['address']['zipcode'] = request.form['zip']
+        filter_params['date']['grade'] = request.form['grade']
+        filter_params['date']['score'] = request.form['score']
+        session['filtered_data'] = restaurant_id.find(filter_params, {'_id': 0})
 
 client.close()
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
