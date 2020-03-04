db770c611f2bc500d33a7977b776574ce8c8b885
Winston Peng <panda501wp@gmail.com>
2020-03-04 11:38:18 +0000

    Add makefile to build actual file, ignore the website b/c didn't implement int

--- spring/10_mongo/utl/funcs.py
+++ spring/10_mongo/utl/funcs.py
@@ -1,7 +1,8 @@
 import urllib.request
-import json
 import pymongo
 import bson.json_util
+import pprint as pp
+import json
 from pprint import pprint
 
 
@@ -10,12 +11,43 @@ db = client['10_mongo']
 senators = db['senators']
 
 def fill_database():
-    u = urllib.request.urlopen('https://www.govtrack.us/api/v2/role?current=true&role_type=senator')
-    response = u.read()
-    data = json.loads(reponse)
-    senators.insert_one(json_util.loads(line))
+    response = urllib.request.urlopen(
+        'https://www.govtrack.us/api/v2/role?current=true&role_type=senator')\
+                             .read()
+    data = json.loads(response)['objects']
+    json_data = json.dumps(data)
+    senators.delete_one(data[0])
+    for senator in data:
+        senators.insert_one(senator)
+    # bson_data = bson.json_util.loads(json_data)
+    # senators.insert_one(bson.json_util.loads(json_data))
 
-if senators.find({}) is None:
+def is_empty_database(db):
+    try:
+        db.find({})[0]
+    except IndexError:
+        return True
+    return False
+
+def print_database(db, filter_params={}, print_params={'_id': 0,
+                                                       'caucus': 0,
+                                                       'title': 0,
+                                                       'title_long': 0}):
+    for line in db.find(filter_params, print_params):
+        print(line)
+
+def cleanse(party=None, gender=None, senator_rank=None):
+    filter_params = {}
+    if not gender is None:
+        filter_params['person'] = {}
+        filter_params['person']['gender'] = gender
+    if not senator_rank is None:
+        filter_params['senator_rank'] = senator_rank
+    if not party is None:
+        filter_params['party'] = party
+    return filter_params
+
+if is_empty_database(senators):
     fill_database()
-
-print(senators.find({}))
+print_database(senators, {'website': 'https://www.coons.senate.gov'})
+# print_database(senators)
